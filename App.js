bd981b2a7f4adcb463230f1ac3e2580781b4c52f
Jonathan D'Souza <jtdsouza@gmail.com>
2023-02-19 10:35:25 -0500

    Flask API Call

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from "react"
 
 export default function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect (() => {
+    fetch("/time").then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    })
+  });
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text> The Time is {currentTime}</Text>
     </View>
   );
 }
